bc575e03b788f5ba152d39d08253aa4c62e71539
Rountman <entonypecka@gmail.com>
2021-11-24 16:00:57 +0100

    Edit code

--- unity/ar/Assets/Scripts/MovingScript.cs
+++ unity/ar/Assets/Scripts/MovingScript.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
-public class SpawnableScript : MonoBehaviour
+public class MovingScript : MonoBehaviour
 {
     [SerializeField]
     ARRaycastManager m_RaycastManager;
-    List<ARRaycastHit> m_Hits = new List<ARRaycastHit>();
+    
     [SerializeField]
     GameObject spawnedPrefab;
+
+    List<ARRaycastHit> m_Hits = new List<ARRaycastHit>();
 
     void Start()
     {
@@ -19,9 +21,7 @@ public class SpawnableScript : MonoBehaviour
     void Update()
     {
         if (Input.touchCount == 0)
-        {
             return;
-        }
 
         if (m_RaycastManager.Raycast(Input.GetTouch(0).position, m_Hits) && Input.GetTouch(0).phase == TouchPhase.Began && m_Hits[0].trackable is ARPlane plane)
         {
